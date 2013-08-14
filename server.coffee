b53c0044ce004278e18e7d8c81a62d2a272bcd3e
Nelson Omuto <nelsonomuto@gmail.com>
2013-08-14 01:16:18 -0500

    GET /getAppInfo returning more details
    GET /getAppInfo returning more details:
    Ex:
    {
    "name": "PicShop - Photo Editor",
    "price": "$0.00",
    "listPrice": "$4.99",
    "youSave": "$4.99 (100%)",
    "rated": "All Ages",
    "reviews": null,
    "imageSrc":
    "http://ecx.images-amazon.com/images/I/71-utGGvjmL._SL500_AA280_.png"
    }
    TODO: return reviews

--- server.coffee
+++ server.coffee
@@ -10,30 +10,54 @@ Express will serve static content from the public directory
 server.use express.static __dirname + '/public'
 
 server.get '/getAppInfo', ( req, res ) ->
-  http.get 'http://www.amazon.com/mobile-apps/b?ie=UTF8&node=2350149011', ( amazonResponse ) ->
-    amazonResponse.setEncoding 'utf8'
-    html = ''
-    amazonResponse.on 'data', (chunk) ->
-      html += chunk
+  getWebsiteHtml 'http://www.amazon.com/mobile-apps/b?ie=UTF8&node=2350149011', ( html ) ->
+    $ = cheerio html
 
+    fullDescription = $.find('.fad-widget-footer-info').find('div').text()
+    title = $.find('.fad-widget-footer-info').find('a').text()
 
-    amazonResponse.on 'end', ->
-      $ = cheerio html
+    ###
+    TODO: use this link to go to app site which has more precise selectors for price and all other info
+    ###
+    link = 'http://www.amazon.com' + $.find('.fad-widget-footer-info').find('a').attr('href')
+    
+    getWebsiteHtml link, ( appHtml ) ->
+      $ = cheerio appHtml
       
-      fullDescription = $.find('.fad-widget-footer-info').find('div').text()
-      title = $.find('.fad-widget-footer-info').find('a').text()
-      
-      ###
-      TODO: use this link to go to app site which has more precise selectors for price and all other info
-      ###
-      link = 'http://www.amazon.com' + $.find('.fad-widget-footer-info').find('a').attr('href')
+      name = $.find('#btAsinTitle').text()
+      price = $.find('#actualPriceValue').text()
+      listPrice = $.find('#listPriceValue').text()
+      youSave = $.find('#youSaveValue').text()
+      rated = $.find('#mas-show-ratings').text()
+      imageSrc = $.find('img#prodImage').attr('src')
+      #TODO: Get Reviews
+      reviews = null
       
       res.json 
-        fullDescription: fullDescription
-        title: title
-        link: link
+        name: name
+        price: price
+        listPrice: listPrice
+        youSave: youSave
+        rated: rated
+        reviews: reviews
+        imageSrc: imageSrc
 
 ###
 Navigate to localhost:3000 to hit /public/index.html
 ###
 server.listen 3000
+
+###
+Abstracting http get request
+###
+getWebsiteHtml = ( url, callback, scope ) ->
+  http.get url, ( urlResponse ) ->
+    urlResponse.setEncoding 'utf8'
+    html = ''
+    
+    urlResponse.on 'data', (chunk) ->
+      html += chunk
+
+
+    urlResponse.on 'end', ->
+      callback.call scope, html
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var cheerio, express, http, server;
+var cheerio, express, getWebsiteHtml, http, server;
 
 express = require('express');
 
@@ -16,27 +16,34 @@ Express will serve static content from the public directory
 server.use(express["static"](__dirname + '/public'));
 
 server.get('/getAppInfo', function(req, res) {
-  return http.get('http://www.amazon.com/mobile-apps/b?ie=UTF8&node=2350149011', function(amazonResponse) {
-    var html;
-    amazonResponse.setEncoding('utf8');
-    html = '';
-    amazonResponse.on('data', function(chunk) {
-      return html += chunk;
-    });
-    return amazonResponse.on('end', function() {
-      var $, fullDescription, link, title;
-      $ = cheerio(html);
-      fullDescription = $.find('.fad-widget-footer-info').find('div').text();
-      title = $.find('.fad-widget-footer-info').find('a').text();
-      /*
-      TODO: use this link to go to app site which has more precise selectors for price and all other info
-      */
+  return getWebsiteHtml('http://www.amazon.com/mobile-apps/b?ie=UTF8&node=2350149011', function(html) {
+    var $, fullDescription, link, title;
+    $ = cheerio(html);
+    fullDescription = $.find('.fad-widget-footer-info').find('div').text();
+    title = $.find('.fad-widget-footer-info').find('a').text();
+    /*
+    TODO: use this link to go to app site which has more precise selectors for price and all other info
+    */
 
-      link = 'http://www.amazon.com' + $.find('.fad-widget-footer-info').find('a').attr('href');
+    link = 'http://www.amazon.com' + $.find('.fad-widget-footer-info').find('a').attr('href');
+    return getWebsiteHtml(link, function(appHtml) {
+      var imageSrc, listPrice, name, price, rated, reviews, youSave;
+      $ = cheerio(appHtml);
+      name = $.find('#btAsinTitle').text();
+      price = $.find('#actualPriceValue').text();
+      listPrice = $.find('#listPriceValue').text();
+      youSave = $.find('#youSaveValue').text();
+      rated = $.find('#mas-show-ratings').text();
+      imageSrc = $.find('img#prodImage').attr('src');
+      reviews = null;
       return res.json({
-        fullDescription: fullDescription,
-        title: title,
-        link: link
+        name: name,
+        price: price,
+        listPrice: listPrice,
+        youSave: youSave,
+        rated: rated,
+        reviews: reviews,
+        imageSrc: imageSrc
       });
     });
   });
@@ -48,3 +55,22 @@ Navigate to localhost:3000 to hit /public/index.html
 
 
 server.listen(3000);
+
+/*
+Abstracting http get request
+*/
+
+
+getWebsiteHtml = function(url, callback, scope) {
+  return http.get(url, function(urlResponse) {
+    var html;
+    urlResponse.setEncoding('utf8');
+    html = '';
+    urlResponse.on('data', function(chunk) {
+      return html += chunk;
+    });
+    return urlResponse.on('end', function() {
+      return callback.call(scope, html);
+    });
+  });
+};
